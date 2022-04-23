48ae803adf271e1dc7bc48eeb4cdfaeb5d7d9766
Aragornz <70964354+aragornz325@users.noreply.github.com>
2022-04-22 23:39:36 -0300

    first commit

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,4 +1,4 @@
-const pg = require('pg')
+const {pg} = require('pg')
 const { Sequelize } = require('sequelize');
 const { config } = require('../config/config');
 const setupModels = require('../db/models/index')
